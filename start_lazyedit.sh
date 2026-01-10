1cb61a9fadc39e184412c76c310c3a20a20a290d
lazyingart <lachlan@lazying.art>
2026-01-10 16:20:38 +0800

    rename lazyedit tmux session

--- start_lazyedit.sh
+++ start_lazyedit.sh
@@ -17,33 +17,33 @@ if [ -z "$CONDA_ENV" ]; then
     CONDA_ENV="lazyedit"
 fi
 
-# Check if the tmux session 'lazyedit' already exists
-tmux has-session -t lazyedit 2>/dev/null
+# Check if the tmux session 'la-lazyedit' already exists
+tmux has-session -t la-lazyedit 2>/dev/null
 
 # Check the exit status of the previous command
 if [ $? != 0 ]; then
     # If the session does not exist, create it
-    tmux new -d -s lazyedit
+    tmux new -d -s la-lazyedit
 
     # Wait a bit to ensure that commands are sent after the session is properly set up
     sleep 2
 
     # Activate the conda environment using the function from config
-    tmux send-keys -t lazyedit "cd \"$LAZYEDIT_DIR\"" C-m
+    tmux send-keys -t la-lazyedit "cd \"$LAZYEDIT_DIR\"" C-m
     if [ -f "$BASHRC_PATH" ]; then
-        tmux send-keys -t lazyedit "source \"$BASHRC_PATH\"" C-m
+        tmux send-keys -t la-lazyedit "source \"$BASHRC_PATH\"" C-m
     fi
     if [ -n "$CONDA_PATH" ] && [ -f "$CONDA_PATH" ]; then
-        tmux send-keys -t lazyedit "source \"$CONDA_PATH\"" C-m
+        tmux send-keys -t la-lazyedit "source \"$CONDA_PATH\"" C-m
     fi
-    tmux send-keys -t lazyedit "conda activate \"$CONDA_ENV\"" C-m
+    tmux send-keys -t la-lazyedit "conda activate \"$CONDA_ENV\"" C-m
 
     # Wait for the conda environment to activate
     sleep 5
 
     # Execute the script
-    tmux send-keys -t lazyedit "cd $LAZYEDIT_DIR" C-m
-    tmux send-keys -t lazyedit "python app.py $APP_ARGS" C-m
+    tmux send-keys -t la-lazyedit "cd $LAZYEDIT_DIR" C-m
+    tmux send-keys -t la-lazyedit "python app.py $APP_ARGS" C-m
 else
-    echo "Tmux session 'lazyedit' already exists."
+    echo "Tmux session 'la-lazyedit' already exists."
 fi
